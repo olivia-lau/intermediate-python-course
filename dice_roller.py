a4b79b0736f8c6bb54e76c1a70181241667d9c9c
olivia <olivialauu@gmail.com>
2021-03-07 12:33:01 -0500

    Print random roll

--- dice_roller.py
+++ dice_roller.py
@@ -1,7 +1,11 @@
 #!/usr/bin/env python3
+import random
+
+
 
 def main():
-  print('You rolled a die')
+  roll = random.randint(1, 6)
+  print(f'You rolled a {roll}')
 
 if __name__== "__main__":
   main()
